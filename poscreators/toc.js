2f42cbf8b83c275b8ffcb4d73029382adc6d98c2
deboragracio <debora.gracio@fiskaltrust.services>
2026-05-11 14:18:24 +0200

    First draft
    https://github.com/fiskaltrust/engineering/issues/1118

--- poscreators/toc.js
+++ poscreators/toc.js
@@ -5,7 +5,6 @@ module.exports = [
     items: [
       "poscreators/getting-started/get-started",
       "poscreators/getting-started/portal-registration",
-
       {
         type: "category",
         collapsed: true,
@@ -15,10 +14,18 @@ module.exports = [
           "poscreators/getting-started/integration-checklist",
         ],
       },
-
       "poscreators/getting-started/onboarding-posdealers",
     ],
   },
+  {
+  type: "category",
+  label: "PosSystem API",
+  collapsed: true,
+    items: [
+      "poscreators/middleware-doc/possystem-api/introduction",
+      "poscreators/middleware-doc/possystem-api/migration",
+    ],
+},
   {
     type: "category",
     collapsed: true,
