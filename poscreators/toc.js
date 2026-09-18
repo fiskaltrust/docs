b543e9b109c82832100f0fd9c7ad2ef99a4731e0
Pawel van de Sanden <karczewskipawel.j@gmail.com>
2026-09-18 11:58:09 +0200

    Add Android IPC Transport documentation

--- poscreators/toc.js
+++ poscreators/toc.js
@@ -24,6 +24,7 @@ module.exports = [
     collapsed: true,
     items: [
       "poscreators/middleware-doc/possystem-api/introduction",
+      "poscreators/middleware-doc/possystem-api/android-ipc",
       "poscreators/middleware-doc/possystem-api/android-intent",
       "poscreators/middleware-doc/possystem-api/migration-guide",
     ],
